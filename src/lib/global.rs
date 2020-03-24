4a607b657d85a63a27a8ef6493a66d1455cf731f
Vin Singh <theshampoofactory@gmail.com>
2020-03-24 20:29:11 +0000

    Correct MAX_ENC_LEN.

--- src/lib/global.rs
+++ src/lib/global.rs
@@ -9,4 +9,4 @@ pub const INFO_KEY: &[u8] = b"KEY info default";
 pub const INFO_MAC: &[u8] = b"MAC info default";
 pub const SALT_KDF: &[u8] = b"KDF salt default";
 
-pub const MAX_ENC_LEN: u64 = 1024 * 1024 * 1024;
+pub const MAX_ENC_LEN: u64 = 1024 * 1024 * 1024 * 1024;
